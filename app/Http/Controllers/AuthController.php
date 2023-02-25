078d2e49a86e429b16225af7cc6188b4e3df5fbd
Sadrul <Sadrul@gmail.com>
2023-02-25 09:35:10 +0600

    Auth Sanctum generate

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+class AuthController extends Controller
+{
+    public function registration(Request $request){
+        
+    }
+    public function login(Request $request){
+
+    }
+    public function logout(Request $request){
+
+    }
+   
+}
