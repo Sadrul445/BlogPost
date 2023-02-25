3e177cfa47f836497c888f96f497334408e607ec
Sadrul <Sadrul@gmail.com>
2023-02-25 11:10:27 +0600

    Registration and Login Api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,14 +4,56 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 class AuthController extends Controller
 {
     public function registration(Request $request){
-        
+        //validation
+        $data = $request->validate(
+            [
+                "name"=>"required|string",
+                "email"=>"required|string|unique:users,email",
+                "password"=>"required|string|confirmed",
+            ]
+            );
+            //create_user
+            $user  = User::create([
+                "name"=> $data["name"],
+                "email"=> $data["email"],
+                "password"=> bcrypt($data["password"])
+            ]);
+            //token
+
+            $token=$user->createToken('myBlogWebToken')->plainTextToken;
+            $response = [
+                "user" => $user,
+                "token"=> $token
+            ];
+            return Response($response,201);
     }
     public function login(Request $request){
-
-    }
+        $data = $request->validate(
+            [
+                "email"=>"required|string",
+                "password"=>"required|string"
+            ]
+            );
+            //email check
+            $user=User::where("email",$data["email"])->first();
+            //check password
+            if(!$user || !Hash::check($data["password"],$user->password)){
+                return response([
+                    "message" => "Bad Credentials",
+                ],401);
+            }
+            // //token
+            $token=$user->createToken('myBlogWebToken')->plainTextToken;
+            $response=[
+                "user" =>$user,
+                "token"=>$token
+            ];
+            return Response($response,201);
+        }
     public function logout(Request $request){
 
     }
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/registration',[AuthController::class,'registration']); //completed
+Route::post('/login',[AuthController::class,'login']); //completed
 
 Route::get('/posts',[PostController::class,'index_post_api']); //completed
 Route::post('/posts',[PostController::class,'store']); //completed
@@ -15,12 +19,3 @@ Route::post('/posts/{id}',[PostController::class,'update_post_api']); //complete
 Route::get('/posts/{id}',[PostController::class,'show']); //completed
 Route::delete('/posts/delete/{id}',[PostController::class,'destroy_post_api']); //completed
 
-// Route::middleware('auth')->group(function (){
-//     Route::get('/dashboard',[DashboardController::class,'show_post'])->name('dashboard');
-
-
-//     Route::post('/post',[PostController::class,'create'])->name('post_create');
-//     Route::get('/post/edit/{id}',[PostController::class,'edit'])->name('post_edit');
-//     Route::put('/post/edit/{id}',[PostController::class,'update'])->name('post_update');
-//     Route::get('/post/delete/{id}',[PostController::class,'destroy'])->name('post_delete');
-// });
