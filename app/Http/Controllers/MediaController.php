e9c4c9e5a9bba45afdd8ea86205a88c92f2637a4
Sadrul <Sadrul@gmail.com>
2023-03-02 14:50:10 +0600

    decoded the encoded image

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -6,6 +6,7 @@ use App\Models\Media;
 use Illuminate\Http\Request;
 // use App\Http\Requests\MediaStoreRequest;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class MediaController extends Controller
 {
@@ -19,33 +20,38 @@ class MediaController extends Controller
         //validation
         $request->validate(
             [
-                'title'=>'required|string',
-                'image'=>'required',
-                'newspaper_name'=>'required',
-                'newspaper_title'=>'required',
-                'newspaper_url'=>'required',
-                'newspaper_description'=>'required',
+                'title' => 'required|string',
+                'image' => 'required',
+                'newspaper_name' => 'required',
+                'newspaper_title' => 'required',
+                'newspaper_url' => 'required',
+                'newspaper_description' => 'required',
             ]
         );
-        
-        //store_uploaded_file
-        $image = $request->file('image')->store('media_images', 'public');
+        // Check if the file is present in the request
+        // if (!$request->hasFile('image')) {
+        //     return response()->json([
+        //         'message' => 'Image not found in request',
+        //         'status' => 'error',
+        //     ], 400);
+        // }
 
-        //get_the_full_file_path
-        $filePath = storage_path('app/public/' . $image);
+        //decode the base64-encoded image data
+        $decodedImage = base64_decode($request->input('image'));
 
-        //convert_to_base64
-        $base64=base64_encode(file_get_contents($filePath));
+        //store the decoded image data in the "decoded_media_images" directory
+        $imagePath = 'decoded_media_images/' . time() . '.png';
+        Storage::disk('public')->put($imagePath, $decodedImage);
 
         //create_media
-        $media= Media::create(
+        $media = Media::create(
             [
-                'title'=>$request->title,
-                'image'=>$base64,
-                'newspaper_name'=>$request->newspaper_name,
-                'newspaper_url'=>$request->newspaper_url,
-                'newspaper_title'=>$request->newspaper_title,
-                'newspaper_description'=>$request->newspaper_description
+                'title' => $request->title,
+                'image' => $imagePath,
+                'newspaper_name' => $request->newspaper_name,
+                'newspaper_url' => $request->newspaper_url,
+                'newspaper_title' => $request->newspaper_title,
+                'newspaper_description' => $request->newspaper_description
             ]
         );
         return response()->json([
@@ -54,33 +60,37 @@ class MediaController extends Controller
             'media' => $media
         ], 200);
     }
-    public function show_single_media(Request $request,$id){
+    public function show_single_media(Request $request, $id)
+    {
         return response()->json(Media::findOrFail($id));
     }
-    public function show_media(Request $request){
+    public function show_media(Request $request)
+    {
         return response()->json(Media::all());
     }
-    public function update_media(Request $request,$id){
-            $media = Media::findOrFail($id);
-            $media->title=$request->title;
+    public function update_media(Request $request, $id)
+    {
+        $media = Media::findOrFail($id);
+        $media->title = $request->title;
 
-            $updated_image_location = public_path("storage\\". $media->image);
-            if($request->hasFile('new_image')){
-                if(File::exists($updated_image_location)){
-                    File::delete($updated_image_location);
-                }
-                $image=$request->file('new_image')->store('updated_media_images','public');
-            }else{
-                $image=$request->image;
-                }
+        $updated_image_location = public_path("storage\\" . $media->image);
+        if ($request->hasFile('new_image')) {
+            if (File::exists($updated_image_location)) {
+                File::delete($updated_image_location);
+            }
+            $image = $request->file('new_image')->store('updated_media_images', 'public');
+        } else {
+            $image = $request->image;
+        }
 
-            $media->newspaper_name=$request->newspaper_name;
-            $media->newspaper_url=$request->newspaper_url;
-            $media->newspaper_description=$request->newspaper_description;
-            $media->save();
-            return $media;
+        $media->newspaper_name = $request->newspaper_name;
+        $media->newspaper_url = $request->newspaper_url;
+        $media->newspaper_description = $request->newspaper_description;
+        $media->save();
+        return $media;
     }
-    public function delete_media(Request $request, $id){
+    public function delete_media(Request $request, $id)
+    {
         return Media::destroy($id);
     }
 }
